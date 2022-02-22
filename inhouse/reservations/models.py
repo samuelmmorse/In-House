be03fce3aefff100d6370cdcc2060090090f5f94
KBendo98 <kleitionbendo98@gmail.com>
2022-02-21 20:26:59 -0500

    Adding a new reservation makes an insertion on DB

--- inhouse/reservations/models.py
+++ inhouse/reservations/models.py
@@ -2,10 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Reservation(models.Model):
-    course = models.TextField()
     topic = models.TextField()
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+    duration = models.TextField(default="15 Minutes")
     student = models.TextField()
     helper = models.TextField()
-    feedback = models.TextField()
+    feedback = models.TextField(default="")
--- inhouse/reservations/views.py
+++ inhouse/reservations/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from reservations.models import Reservation
 
 # Create your views here.
 def new_help_session(request):
+    if request.method=="POST":
+        helper = request.POST['helper']
+        topic = request.POST['topic']
+        time = request.POST['time']
+        
+        ins = Reservation(topic=topic, duration=time, student='Ben', helper=helper)
+        ins.save()
+
     return render(request, 'new_help_session_student_view.html')
