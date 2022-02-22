bbdaf37f9685cdf986dea738dfd919922ea1f973
MainMan12 <60482423+MainMan12@users.noreply.github.com>
2022-02-21 20:35:30 -0500

    Update manage.py

---
+++ inhouse/boardmanlab/urls 2.py
@@ -0,0 +1,17 @@
+from unicodedata import name
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path('/home', views.index, name='index'),
+    path('', views.login, name='login'),
+    path('/helpsessions', views.helpsessions, name='helpsessions'),
+    path('calendar', views.calendar, name='calendar'),
+    path('calendarMonth/<int:year>/<int:month>/<int:day>/', views.calendarMonth, name='calendarMonth'),
+    path('calendarDay/<int:year>/<int:month>/<int:day>/', views.calendarDay, name='calendarDay'),
+    path('/error', views.error, name='error'),
+    path('/accounts', include('allauth.urls')),
+    
+    path('myAccount', views.myAccount, name='myAccount')
+]
---
+++ inhouse/boardmanlab/views 2.py
@@ -0,0 +1,96 @@
+from django.shortcuts import render
+from oauth2_provider.views.generic import ProtectedResourceView
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+from datetime import datetime
+import calendar
+cal = calendar.Calendar()
+cal.setfirstweekday(calendar.SUNDAY)
+
+
+# Create your views here.
+
+@login_required()
+def myAccount(request):
+    return render(request, 'myAccount.html')
+
+
+@login_required()
+def index(request):
+    return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+@login_required()
+def calendarMonth(request, year, month, day):
+    if year == 0 and month == 0:
+        year = datetime.now().year
+        month = datetime.now().month
+        day = datetime.now().day
+    if day == 0 and month == datetime.now().month:
+        day = datetime.now().day
+    context = {
+        "day_string_list": [6,0,1,2,3,4,5],
+        "day": day,
+        "month": month,
+        "year": year,
+        "month_obj": cal.monthdayscalendar(year, month),
+    }
+    return render(request, 'calendarMonth.html', context)
+
+@login_required()
+def calendarDay(request, year, month, day):
+    if year == 0 and month == 0:
+        year = datetime.now().year
+        month = datetime.now().month
+        day = datetime.now().day
+    if day == 0 and month == datetime.now().month:
+        day = datetime.now().day
+    context = {
+        "day_string_list": [6,0,1,2,3,4,5],
+        "day": day,
+        "month": month,
+        "year": year,
+        "month_obj": cal.monthdayscalendar(year, month),
+    }
+    return render(request, 'calendarDay.html', context)
+
+@login_required()
+def helpsessions(request):
+    return render(request, 'helpSessions.html')
+
+def error(request):
+    return render(request, 'error.html')
+
+
+#AUTHORIZED ONLY VIEWS
+class ApiEndpoint(ProtectedResourceView):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse(
+            'Hello there! You are acting on behalf of "%s"\n'
+            % (request.user))
+
+
+class Home(TemplateView):
+    template_name = 'home.html'
+
+
+
+# Error Handling
+def error_404(request, exception):
+        data = {}
+        return render(request,'errors/404.html', data)
+
+def error_500(request):
+        data = {}
+        return render(request,'errors/500.html', data)
+
+def error_400(request, exception):
+        data = {}
+        return render(request,'errors/400.html', data)
+
+def error_403(request,  exception):
+        data = {}
+        return render(request,'errors/403.html', data)
